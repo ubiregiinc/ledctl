4c9c39e3365c3f1a415d88d67e405d6210285d81
Soutaro Matsumoto <matsumoto@soutaro.com>
2014-02-12 02:02:29 +0900

    Add write to write file

--- lib/led-control-tool.rb
+++ lib/led-control-tool.rb
@@ -4,3 +4,9 @@ require "socket"
 require "led-control-tool/server"
 require "led-control-tool/client"
 require "led-control-tool/cli"
+
+class Pathname
+	def write(*args)
+		self.open('w') {|io| io.write(*args) }
+	end
+end
