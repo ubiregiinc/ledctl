6425d25b17d81e096acdb80153ebf167555413d5
Soutaro Matsumoto <matsumoto@soutaro.com>
2014-02-12 03:47:55 +0900

    Daemonize

--- lib/led-control-tool/cli.rb
+++ lib/led-control-tool/cli.rb
@@ -8,7 +8,22 @@ module LEDControlTool
     def server(pinno)
     	LEDControlTool::Server.new(:pinno => pinno.to_s, :socket => options[:sock]).start do |server|
         if options[:daemon]
-          puts "Should daemonize here"
+          pidfile = Pathname.new("/var/run/ledctl.pid")
+
+          if pidfile.file?
+            puts "Aborting: #{pidfile} already exist; maybe already running?"
+            exit
+          end
+
+          Process.daemon
+
+          pidfile.open('w') do |io|
+            io.puts(Process.pid)
+          end
+
+          at_exit do
+            pidfile.unlink if pidfile.file?
+          end
         end
 
         case options[:initial]
